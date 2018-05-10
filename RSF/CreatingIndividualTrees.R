f2aeb7e333a456a85bd20524ca981449898d5a4a
Humza Haider <humza@haiderstats.com>
2018-05-10 16:53:51 -0600

    KM trees now weighted.

--- RSF/CreatingIndividualTrees.R
+++ RSF/CreatingIndividualTrees.R
@@ -1,14 +1,12 @@
 #Started on Monday May 7, 2018 by Humza Haider. The goal of this exploratory file 
 #is to discern how to retrieve the survival curves from each node of each tree in 
 #a random forest. 
-
 library(randomForestSRC);
 library(caret)
 library(data.table)
 library(zoo)
 library(parallel)
-no_cores <- detectCores()
-cl <- makeCluster(no_cores, type = "FORK")
+
 
 data(wihs, package = "randomForestSRC")
 wihs = wihs[-which(wihs$status ==2),]
@@ -21,14 +19,21 @@ wihs.obj <- rfsrc(Surv(time, status) ~ ., train, nsplit = 3, ntree = ntree,
                   membership = T)
 #Get the training nodes
 trainMembership = as.data.frame(wihs.obj$membership)
+no_cores <- detectCores()
+cl <- makeCluster(no_cores-1, type = "FORK")
 listOfCurves =  parLapply(cl,1:ntree, function(x){
                           treeInfo = trainMembership[,x]
                           uniqueNodes= unique(treeInfo)
-                          listOfStuff = lapply(uniqueNodes, function(y){summ = summary(survfit(Surv(time,status)~1, train[which(treeInfo == y),]));
-                          node = y;
-                          list(summ, node)})
-                          plyr::ldply(listOfStuff, function(z) cbind.data.frame(TreeID = x, NodeID = z[[2]],time = c(0,z[[1]]$time), survival = c(1,z[[1]]$surv)))
+                          listOfStuff = lapply(uniqueNodes, function(y){
+                                               survMod = survfit(Surv(time,status)~1, train[which(treeInfo == y),])
+                                               node = y
+                                               list(survMod, node)})
+                          plyr::ldply(listOfStuff, function(z){
+                            summ = summary(z[[1]])
+                            cbind.data.frame(TreeID = x, NodeID = z[[2]],time = c(0,summ$time), survival = c(1,summ$surv), N = z[[1]]$n, numEvents = sum(z[[1]]$n.event))
                           })
+                          })
+stopCluster(cl)
 KMCurves = plyr::ldply(listOfCurves, rbind)
 
 DT = data.table(KMCurves)
@@ -46,7 +51,7 @@ for(i in 1:(ncol(predictMe))){
   
   #We add some time at the end so the last two time points don't end up in the same interval.
   times =c(times,times[length(times)] + 1e-6)
-  cuttedTimes = cut(times[-62],times,include.lowest = T,right=F)
+  cuttedTimes = cut(times[-length(times)],times,include.lowest = T,right=F)
   personCurves[, cutTime := cut(time, times, include.lowest = T,right=F)]
   
   toJoin = data.table(TreeID = rep(1:100,each = length(cuttedTimes)),cutTime = cuttedTimes)
@@ -55,15 +60,18 @@ for(i in 1:(ncol(predictMe))){
   #Sets up ordering for na.locf
   setkey(DTFull, TreeID, cutTime)
   #Adds the most recent survival probability to NA values going forward.
+  DTFull[,totalEvents := sum(DTFull[DTFull[,.I[1], c('TreeID','numEvents')]$V1,numEvents], na.rm=T)]
+  DTFull[,survival := survival*(numEvents/totalEvents)]
+  
   DTFull[,survival := na.locf(survival)]
-  IndividualCurve = DTFull[, mean(survival), by =cutTime]
+  IndividualCurve = DTFull[, sum(survival), by =cutTime]
   dats = data.table(ID = i,Time = times[-length(times)], Survival = IndividualCurve[,V1])
   patientList = rbindlist(list(patientList,dats))
   if(i %% round(ncol(predictMe)/10) == 0){
     print(paste(i*100/ncol(predictMe),"%",sep = ""))
   }
 }
-smallPat = patientList[patientList$ID %in% 1:10,]
+smallPat = patientList[patientList$ID %in% 40:60,]
 smallPat$ID = as.factor(smallPat$ID)
 ggplot(smallPat,aes(Time,Survival,color=ID, group =ID))+
         geom_point()+
