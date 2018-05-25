cc61dc65fb470b015629eb2f9add7abb325b4364
Humza Haider <humza@haiderstats.com>
2018-05-25 11:16:09 -0600

    Code for initial validation checks and cleaning.

--- ISDEvaluationPipeline/ValidateCleanCV/validateAndClean.R
+++ ISDEvaluationPipeline/ValidateCleanCV/validateAndClean.R
@@ -13,16 +13,57 @@
 #Input: Survival Dataset
 #Output: Survival Dataset which has been validated and had minor cleaning.
 ############################################################################################################################################
+validateAndClean = function(survivalDataset){
+  validatedData = validate(survivalDataset)
+  cleanData = clean(validatedData)
+  return(cleanData)
+}
 
+validate = function(survivalDataset){
+  if(is.null(survivalDataset) || nrow(survivalDataset) == 0)
+    stop("You must enter a nonempty survival dataset.")
+  if(!"time" %in% names(survivalDataset))
+    stop("The variable 'time' in not included in the given dataset.")
+  if(!"delta" %in% names(survivalDataset))
+    stop("The variable 'delta' in not included in the given dataset.")
+  survivalDataset$time = as.numeric(survivalDataset$time)
+  survivalDataset$delta = as.numeric(survivalDataset$delta)
+  if(any(is.na(survivalDataset$time))){
+    warning("'time' includes NA values. These rows have been removed.")
+    NAtimeIndex = which(is.na(survivalDataset$time))
+    survivalDataset = survivalDataset[-NAtimeIndex,]
+  }
+  if(any(is.infinite(survivalDataset$time))){
+    warning("'time' includes Inf values. These rows have been removed.")
+    InftimeIndex = which(is.infinite(survivalDataset$time))
+    survivalDataset = survivalDataset[-InftimeIndex,]
+  }
+  if(any(is.na(survivalDataset$delta))){
+    warning("'delta' includes NA values. These rows have been removed.")
+    NAdeltaIndex = which(is.na(survivalDataset$delta))
+    survivalDataset = survivalDataset[-NAdeltaIndex,]
+  }
+  if(any(is.infinite(survivalDataset$delta))){
+    warning("'delta' includes Inf values. These rows have been removed.")
+    InfdeltaIndex = which(is.infinite(survivalDataset$delta))
+    survivalDataset = survivalDataset[-InfdeltaIndex,]
+  }
+  if(length(unique(survivalDataset$delta)) >2)
+    stop("'delta' contains more than two unique values. 'delta' should only contain 0 and 1.")
+  if(!all(sort(unique(survivalDataset$delta)) == c(0,1)))
+    stop("'delta' contains values other than 0 and 1. 0 should indicate RIGHT censoring and 1 should indicate the event occuring.")
+  return(survivalDataset)
+}
 
-
-
-
-
-
-
-
-
-
-
+clean = function(survivalDataset){
+  #Remove columns with over 25% of data missing.
+  naProp = apply(survivalDataset, 2, function(x) sum(is.na(x))/nrow(survivalDataset))
+  toRemove = which(naProp > 0.25) 
+  if(length(toRemove)>0){
+    namesToRemove = names(survivalDataset)[toRemove]
+    survivalDataset = survivalDataset[,-toRemove]
+    warning(paste("The variable(s)",paste(namesToRemove, collapse = ", "),"contained over 25% NA values. They have been removed.", sep = " "))
+  }
+  return(survivalDataset)
+}
 
