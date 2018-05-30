e2b0ebd5a0eb4083455c6d36f838345e31f2a298
Humza Haider <humza@haiderstats.com>
2018-05-30 12:01:53 -0600

    Added documentation for  One Calibration.

--- ISDEvaluationPipeline/Evaluations/OneCalibration.R
+++ ISDEvaluationPipeline/Evaluations/OneCalibration.R
@@ -4,22 +4,29 @@
 #Email: hshaider@ualberta.ca
 
 #Purpose and General Comments:
-#This file was created to implement one calibration as an evaluation metric for individual survival curves.
+#This file was created to implement one calibration as an evaluation metric for individual survival curves. There are three different
+#implementations of one calibration: Uncensored, Fractional, and BucketKM.
+#Uncensored: This method places all indiviuals into decile buckets and then removes all censored individuals.
+#Fractional: This method builds a KM curve from the training data and then uses the KM predictions of the time of interest for observed number
+#of events for censored patients. Specifically we have that P(T>t* | T>c) = P(T>t* ^ T >c)/P(T>c) = P(T>t*)/P(T>c). P is generated using KM.
+#BucketKM: This method is known as the D'Agostino-Nam method and builds a KM curve within each bucket and uses the KM estimate of t* as the
+#value for the observed number of events. For numBuckets <= 15 this follows a chi.square statistic with Numbucket-1 degrees of freedom, 
+#For numBuckets > 15 it follows chi-squared with numBuckets -2 degrees of freedom.
 #Input 1: A list of (1) a matrix of survival curves, and (2) the true death times and event/censoring indicator (delta =1 implies death/event).
 #Output: The desired L-measure value.
 ##############################################################################################################################################
 #Library Dependencies
 #We use this for the sindex function.
 library(prodlim)
-#We use this for ldply, a combiner of lists.
+#We use this for group_by, summarise, and mutate.
 library(dplyr)
-#We use this for hoslem.test
-library(ResourceSelection)
+
 #Helper Functions: predictProbabilityFromCurve(survivalCurve,predictedTimes, timeToPredict)
 source("Evaluations/EvaluationHelperFunctions.R")
 
-#type %in% c("Uncensored","Fractional","BucketKM")
-OneCalibration = function(survMod, timeOfInterest = NULL, type, numBuckets){
+OneCalibration = function(survMod, timeOfInterest = NULL, type = "BucketKM", numBuckets = 10){
+  if(!type %in% c("Uncensored","Fractional","BucketKM"))
+    stop("Please enter one of 'Uncensored','Fractional','BucketKM' for type.")
   predictedTimes = survMod[[1]]$time
   survivalCurves = survMod[[1]][-1] #removes predicted times.
   trueDeathTimes = survMod[[2]]$time
@@ -31,14 +38,19 @@ OneCalibration = function(survMod, timeOfInterest = NULL, type, numBuckets){
                               function(index) predictProbabilityFromCurve(survivalCurves[,index],
                                                                           predictedTimes,
                                                                           timeOfInterest)))
-  orderPredictions = order(predictions)
+  #We need to divide the number of predictions into as equal size buckets as possible. The formula for this can be found here:
   #https://math.stackexchange.com/questions/199690/divide-n-items-into-m-groups-with-as-near-equal-size-as-possible
   numberOfBucketsWithExtra = length(predictions) %% numBuckets
   numberOfBucketsWithoutExtra = numBuckets - numberOfBucketsWithExtra
+  #We want buckets to randomly be assigned their respective number of predictions, but be consistent with the same input.
+  #E.g. for 3 buckets with size 5 and 2 buckets of size 4 we don't necasarrily want 5,5,5,4,4. We want them to be random. We use sample().
   set.seed(42)
   bucketSizes = sample(c(rep(floor(length(predictions)/numBuckets)+1, numberOfBucketsWithExtra),
                          rep(floor(length(predictions)/numBuckets), numberOfBucketsWithoutExtra)))
   bucketLabel = rep(1:numBuckets, times = bucketSizes)
+  
+  #We are going to put the survival probabilities into deciles so we get the order of indexs here.
+  orderPredictions = order(predictions)
   orderedPredictionFrame = data.frame(prob = predictions[orderPredictions],
                                       label = bucketLabel,
                                       delta = censorStatus[orderPredictions],
@@ -48,6 +60,9 @@ OneCalibration = function(survMod, timeOfInterest = NULL, type, numBuckets){
                          delta = censorStatus[orderPredictions])
   pval = switch(type,
                 Uncensored = {
+                  #Read the following code as taking our predicted probabilities, placing them into buckets, 
+                  #taking only uncensored individuals at the time of interest and then taking there mean. All other piping operations should
+                  #be read in a similar fashion.
                   bucketSurvival = orderedPredictionFrame  %>%
                     group_by(label) %>% 
                     filter(delta ==1 | (delta ==0 & time >= timeOfInterest)) %>%
@@ -66,6 +81,7 @@ OneCalibration = function(survMod, timeOfInterest = NULL, type, numBuckets){
                   bucketSurvival = orderedPredictionFrame  %>% 
                     group_by(label) %>%
                     summarise(survivalPrediction = mean(prob))
+                  #The Fractional method has three different cases to consider for the observed quantity of an individual.
                   numDied = timeFrame  %>%
                     #Case 1: Uncensored and died before time of interest, cost = 1.
                     #Case 2: Censored before time of interest, use fractional person method for cost. 
@@ -80,6 +96,8 @@ OneCalibration = function(survMod, timeOfInterest = NULL, type, numBuckets){
                   HLStat = sum((observed-expected)^2/(bucketSizes*(1-bucketSurvival$survivalPrediction)*bucketSurvival$survivalPrediction))
                   pval = 1-pchisq(HLStat, numBuckets-2)
                 },
+                #For a good discusion of this method, also known as the D'Agnostio-Nam tranlation/method see
+                #Section 2.2 of this thesis paper: Hosmer-Lemeshow goodness-of-fit test: Translations to the Cox Proportional Hazards Model
                 BucketKM = {
                   bucketSurvival = orderedPredictionFrame  %>%
                     group_by(label) %>%
@@ -90,7 +108,8 @@ OneCalibration = function(survMod, timeOfInterest = NULL, type, numBuckets){
                   observed = numDied$deadCount
                   expected = 1-bucketSurvival$survivalPrediction
                   HLStat = sum((bucketSizes*(observed-expected)^2)/((1-bucketSurvival$survivalPrediction)*bucketSurvival$survivalPrediction))
-                  pval = 1-pchisq(HLStat, numBuckets-1)
+                  DoF = ifelse(numBuckets > 15, numBuckets -2, numBuckets-1)
+                  pval = 1-pchisq(HLStat, DOF)
                 })
   return(pval)
 }
