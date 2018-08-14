826e28cac78d7a5ae47cb2973f389c1ac5ded2b3
Humza Haider <humza@haiderstats.com>
2018-08-14 09:39:09 -0600

    Include space breaks for warnings and turn variables with unique values of c(-1,0) and c(0,1) to factors.

--- ISDEvaluationPipeline/ValidateCleanCV/validateAndClean.R
+++ ISDEvaluationPipeline/ValidateCleanCV/validateAndClean.R
@@ -25,11 +25,11 @@ validateAndClean = function(survivalDataset, imputeZero=T){
 
 validate = function(survivalDataset,imputeZero){
   if(is.null(survivalDataset) || nrow(survivalDataset) == 0)
-    stop("You must enter a nonempty survival dataset.")
+    stop("You must enter a nonempty survival dataset.\n")
   if(!"time" %in% names(survivalDataset))
-    stop("The variable 'time' in not included in the given dataset.")
+    stop("The variable 'time' in not included in the given dataset.\n")
   if(!"delta" %in% names(survivalDataset))
-    stop("The variable 'delta' in not included in the given dataset.")
+    stop("The variable 'delta' in not included in the given dataset.\n")
   survivalDataset$time = as.numeric(survivalDataset$time)
   survivalDataset$delta = as.numeric(survivalDataset$delta)
   #We want to change the ordering so that the time column is first and the delta column is second.
@@ -37,41 +37,41 @@ validate = function(survivalDataset,imputeZero){
   deltaIndex = which(names(survivalDataset) == "delta")
   survivalDataset = survivalDataset[,c(timeIndex,deltaIndex, (1:ncol(survivalDataset))[-c(timeIndex,deltaIndex)])]
   if(any(is.na(survivalDataset$time))){
-    warning("'time' includes NA values. These rows have been removed.")
+    warning("'time' includes NA values. These rows have been removed.\n")
     NAtimeIndex = which(is.na(survivalDataset$time))
     survivalDataset = survivalDataset[-NAtimeIndex,]
   }
   if(any(is.infinite(survivalDataset$time))){
-    warning("'time' includes Inf values. These rows have been removed.")
+    warning("'time' includes Inf values. These rows have been removed.\n")
     InftimeIndex = which(is.infinite(survivalDataset$time))
     survivalDataset = survivalDataset[-InftimeIndex,]
   }
   if(any(survivalDataset$time <0)){
-    warning("'time' includes negative values. These rows have been removed.")
+    warning("'time' includes negative values. These rows have been removed.\n")
     nonPosTimeIndex = which(survivalDataset$time <0)
     survivalDataset = survivalDataset[-nonPosTimeIndex,]
   }
   if(any(survivalDataset$time ==0) & imputeZero){
     imputeVal  =min(survivalDataset$time[survivalDataset$time > 0])/2
     warning(paste("'time' includes 0 valued times. These have been imputed to half the minimum positive time point:",
-                  imputeVal))
+                  imputeVal,"\n"))
     zeroTimeIndex = which(survivalDataset$time ==0)
     survivalDataset[zeroTimeIndex,]$time = imputeVal
   }
   if(any(is.na(survivalDataset$delta))){
-    warning("'delta' includes NA values. These rows have been removed.")
+    warning("'delta' includes NA values. These rows have been removed.\n")
     NAdeltaIndex = which(is.na(survivalDataset$delta))
     survivalDataset = survivalDataset[-NAdeltaIndex,]
   }
   if(any(is.infinite(survivalDataset$delta))){
-    warning("'delta' includes Inf values. These rows have been removed.")
+    warning("'delta' includes Inf values. These rows have been removed.\n")
     InfdeltaIndex = which(is.infinite(survivalDataset$delta))
     survivalDataset = survivalDataset[-InfdeltaIndex,]
   }
   if(length(unique(survivalDataset$delta)) >2)
-    stop("'delta' contains more than two unique values. 'delta' should only contain 0 and 1.")
+    stop("'delta' contains more than two unique values. 'delta' should only contain 0 and 1.\n")
   if(!all(sort(unique(survivalDataset$delta)) == c(0,1)))
-    stop("'delta' contains values other than 0 and 1. 0 should indicate RIGHT censoring and 1 should indicate the event occuring.")
+    stop("'delta' contains values other than 0 and 1. 0 should indicate RIGHT censoring and 1 should indicate the event occuring.\n")
   return(survivalDataset)
 }
 
@@ -81,7 +81,7 @@ clean = function(survivalDataset){
   if(length(allSame)>0){
     namesToRemoveNoVar = names(survivalDataset)[allSame]
     survivalDataset = survivalDataset[,-allSame]
-    warning(paste("The variable(s)",paste(namesToRemoveNoVar, collapse = ", "),"contained only 1 unique value. They have been removed.",
+    warning(paste("The variable(s)",paste(namesToRemoveNoVar, collapse = ", "),"contained only 1 unique value. They have been removed.\n",
                   sep = " "))
   }
   #Remove columns with over 25% of data missing.
@@ -90,8 +90,13 @@ clean = function(survivalDataset){
   if(length(toRemoveNA)>0){
     namesToRemoveNA = names(survivalDataset)[toRemoveNA]
     survivalDataset = survivalDataset[,-toRemoveNA]
-    warning(paste("The variable(s)",paste(namesToRemoveNA, collapse = ", "),"contained over 25% NA values. They have been removed.",
+    warning(paste("The variable(s)",paste(namesToRemoveNA, collapse = ", "),"contained over 25% NA values. They have been removed.\n",
                   sep = " "))
+  }
+  indicatorVariables = sapply(survivalDataset[,-c(1,2)], function(x) all(unique(x)[!is.na(unique(x))] %in% c(0,1)) || all(unique(x)[!is.na(unique(x))] %in% c(-1,0)))
+  if(any(indicatorVariables)){
+  survivalDataset[,-c(1,2)][,indicatorVariables] = lapply(survivalDataset[,-c(1,2)][,indicatorVariables], factor)
+  warning(paste(toString(names(survivalDataset[-c(1,2)][indicatorVariables])), "had only unique values c(0,1) or c(-1,0). These were converted to factors.",sep = " "))
   }
   return(survivalDataset)
 }
