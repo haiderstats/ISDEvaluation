54e2d7ab12e2d25e33774030bdffe4b6e1409623
Humza Haider <humza@haiderstats.com>
2018-08-22 14:28:38 -0600

    Updated default value for normal OneCal() and updated argument names and documentation.

--- ISDEvaluationPipeline/Evaluations/OneCalibration.R
+++ ISDEvaluationPipeline/Evaluations/OneCalibration.R
@@ -7,9 +7,7 @@
 #This file was created to implement one calibration as an evaluation metric for individual survival curves. There are three different
 #implementations of one calibration: Uncensored, Fractional, and BucketKM.
 #Uncensored: This method places all indiviuals into decile buckets and then removes all censored individuals.
-#Fractional: This method builds a KM curve from the training data and then uses the KM predictions of the time of interest for observed number
-#of events for censored patients. Specifically we have that P(T>t* | T>c) = P(T>t* ^ T >c)/P(T>c) = P(T>t*)/P(T>c). P is generated using KM.
-#BucketKM: This method is known as the D'Agostino-Nam method and builds a KM curve within each bucket and uses the KM estimate of t* as the
+#DN: This method is known as the D'Agostino-Nam method and builds a KM curve within each bucket and uses the KM estimate of t* as the
 #value for the observed number of events. For numBuckets <= 15 this follows a chi.square statistic with Numbucket-1 degrees of freedom, 
 #For numBuckets > 15 it follows chi-squared with numBuckets -2 degrees of freedom.
 #Input 1: A list of (1) a matrix of survival curves, and (2) the true death times and event/censoring indicator (delta =1 implies death/event).
@@ -18,21 +16,20 @@
 #Library Dependencies
 #We use this for the Surv function.
 library(survival)
-#We use this for the sindex function.
-library(prodlim)
+
 #We use this for group_by, summarise, and mutate.
 library(dplyr)
 
 #Helper Functions: predictProbabilityFromCurve(survivalCurve,predictedTimes, timeToPredict)
 source("Evaluations/EvaluationHelperFunctions.R")
 
-OneCalibration = function(survMod, timeOfInterest = NULL, type = "BucketKM", numBuckets = 10){
+OneCalibration = function(survMod, timeOfInterest = c(), type = "DN", numBuckets = 10){
   #Being passed an empty model.
   if(is.null(survMod)) return(NULL)
   #Being passed a model that failed.
   suppressWarnings(if(is.na(survMod[[1]])) return(NULL))
-  if(!type %in% c("Uncensored","Fractional","BucketKM"))
-    stop("Please enter one of 'Uncensored','Fractional','BucketKM' for type.")
+  if(!type %in% c("Uncensored","DN"))
+    stop("Please enter one of 'Uncensored','DN' for type.")
   predictedTimes = survMod[[1]]$time
   survivalCurves = survMod[[1]][-1] #removes predicted times.
   trueDeathTimes = survMod[[2]]$time
@@ -41,36 +38,26 @@ OneCalibration = function(survMod, timeOfInterest = NULL, type = "BucketKM", num
   trainingCensorStatus = survMod[[3]]$delta
   #If time is null, we will select the median from the KM curve generated from training instances.
   if(is.null(timeOfInterest)){
-    KMCurve = prodlim(Surv(trainingDeathTimes, trainingCensorStatus)~1)
-    timeOfInterest = tryCatch({
-      quantile(KMCurve,.5)$quantiles.survival$quantile
-    },
-    #Catch the case where there is too many censored patients to generate a KM Curve to the median line.
-    #In this case we use our linear extension to find the median.
-    error = function(e){
-      slope = (1-min(KMCurve$surv))/(0 - max(KMCurve$time))
-      timeOfInterest = (-0.5)/slope
-    })
-  }
-  #Additionally sometimes the quantile function will return NA instead of an error if it still able to produce a lower bound. Here we 
-  #do the same thing as above when we catch the error.
-  if(is.na(timeOfInterest)){
-    slope = (1-min(KMCurve$surv))/(0 - max(KMCurve$time))
-    timeOfInterest = (-0.5)/slope
+    timeOfInterest = unname(quantile(trueDeathTimes,c(.1,.25,.5,.75,.9)))
   }
   predictions = unlist(lapply(seq_along(trueDeathTimes),
                               function(index) predictProbabilityFromCurve(survivalCurves[,index],
                                                                           predictedTimes,
                                                                           timeOfInterest)))
-  pval = binItUp(trueDeathTimes, censorStatus, predictions, type, numBuckets,timeOfInterest)
+  pval = c()
+  for(times in 1:length(timeOfInterest)){
+    #If we have multiple times of interest we need to pull out the predictions for a specific time.
+    predictionsSpecified = predictions[seq(times,length(timeOfInterest)*length(trueDeathTimes),length(timeOfInterest))]
+    pval = c(pval,binItUp(trueDeathTimes,censorStatus, predictionsSpecified, type, numBuckets,timeOfInterest[times]))
+  }
   return(pval)
 }
 
-OneCalibrationCumulative = function(listOfSurvivalModels, timeOfInterest = c(), type = "BucketKM", numBuckets = 10){
+OneCalibrationCumulative = function(listOfSurvivalModels, timeOfInterest = c(), type = "DN", numBuckets = 10){
   if(length(listOfSurvivalModels) ==0) return(NULL)
   suppressWarnings(if(any(unlist(lapply(listOfSurvivalModels, is.na)))) return(NA))
-  if(!type %in% c("Uncensored","Fractional","BucketKM"))
-    stop("Please enter one of 'Uncensored','Fractional','BucketKM' for type.")
+  if(!type %in% c("Uncensored","DN"))
+    stop("Please enter one of 'Uncensored','DN' for type.")
   
   predictedTimes = lapply(seq_along(listOfSurvivalModels), function(x) listOfSurvivalModels[[x]][[1]]$time)
   survivalCurves = lapply(seq_along(listOfSurvivalModels), function(x) listOfSurvivalModels[[x]][[1]][-1])
@@ -117,7 +104,7 @@ binItUp = function(trueDeathTimes,censorStatus, predictions, type, numBuckets,ti
   pval = switch(type,
                 Uncensored = {
                   #Read the following code as taking our predicted probabilities, placing them into buckets, 
-                  #taking only uncensored individuals at the time of interest and then taking there mean. All other piping operations should
+                  #taking only uncensored individuals at the time of interest and then taking their mean. All other following piping operations should
                   #be read in a similar fashion.
                   bucketSurvival = orderedPredictionFrame  %>%
                     group_by(label) %>% 
@@ -132,29 +119,9 @@ binItUp = function(trueDeathTimes,censorStatus, predictions, type, numBuckets,ti
                   HLStat = sum((observed-expected)^2/(bucketSizes*(1-bucketSurvival$survivalPrediction)*bucketSurvival$survivalPrediction))
                   pval = 1-pchisq(HLStat, numBuckets-2)
                 },
-                Fractional = {
-                  KM = prodlim(Surv(time,delta)~1, data = survMod[[3]])
-                  bucketSurvival = orderedPredictionFrame  %>% 
-                    group_by(label) %>%
-                    summarise(survivalPrediction = mean(prob))
-                  #The Fractional method has three different cases to consider for the observed quantity of an individual.
-                  numDied = timeFrame  %>%
-                    #Case 1: Uncensored and died before time of interest, cost = 1.
-                    #Case 2: Censored before time of interest, use fractional person method for cost. 
-                    #Case 3: Either uncensored but died after time of interest or censored at or after time of interest, cost = 0.
-                    mutate(contribution = ifelse(delta == 1 & time <= timeOfInterest, 1,
-                                                 ifelse(delta == 0 & time < timeOfInterest,
-                                                        predict(KM,timeOfInterest)/predict(KM,time,level.chaos=2), 0))) %>%
-                    group_by(label) %>%
-                    summarise(deadCount = sum(contribution))
-                  observed = numDied$deadCount
-                  expected = bucketSizes*(1-bucketSurvival$survivalPrediction)
-                  HLStat = sum((observed-expected)^2/(bucketSizes*(1-bucketSurvival$survivalPrediction)*bucketSurvival$survivalPrediction))
-                  pval = 1-pchisq(HLStat, numBuckets-2)
-                },
                 #For a good discusion of this method, also known as the D'Agostino-Nam tranlation/method see
                 #Section 2.2 of this thesis paper: Hosmer-Lemeshow goodness-of-fit test: Translations to the Cox Proportional Hazards Model
-                BucketKM = {
+                DN = {
                   bucketSurvival = orderedPredictionFrame  %>%
                     group_by(label) %>%
                     summarise(survivalPrediction = mean(prob))
