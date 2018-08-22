a3e7bd61fdea0b3e6c50d59843500dda9ea0d0f5
Humza Haider <humza@haiderstats.com>
2018-08-22 12:56:14 -0600

    Removing censored concordance.

--- ISDEvaluationPipeline/Evaluations/Concordance.R
+++ ISDEvaluationPipeline/Evaluations/Concordance.R
@@ -13,25 +13,19 @@
 #(Concordant Pairs + (Number of Ties/2))/(Concordant Pairs + Discordant Pairs + Number of Ties)
 #Currently a "risk" score for a survival distribution model is considered to be the mean survival time using a spline fit to make a continuous
 #survival distribution. 
-#Input 3: A boolean on whether or not to include censored patients in the calculation of concordance. See the paper for details on how 
-#concordance is calculated for censored individuals.
+#Input 3: A string indicating whether the "Mean" or "Median" should be used to calculate a patient's risk score.
 #Output: The C-index.
 ##############################################################################################################################################
 #Library Dependencies
-#We use this for the prodlim function to make a Kaplan-Meier curve. Specifically, we use prodlim due to its predict function.
-library(prodlim)
 #We use this for the survConcordance function.
 library(survival)
-#We use this for ldply, a combiner of lists.
-library(plyr)
+
 #Helper Functions: predictMeanSurvivalTimeLinear(survivalCurve,predictedTimes)
 source("Evaluations/EvaluationHelperFunctions.R")
 
-#The following function is split into 3 parts. Part 1 retrieves all the relevant pieces from the passed in survMod object, e.g. the survival
-#curves and the true death times of test subjects. Part 2 is only used if we are to include concordance calculations for two censored patients,
-#see the paper for the details on this calculation. Part 3 uses survConcordance to calculate classical concordance measures. We then 
-#add in the censored piece from part 2 if includeCensored == True. Additionally, this is were tied data is handled.
-Concordance = function(survMod, ties = "None",includeCensored =F, method = "Mean"){
+#The following function is split into 2 parts. Part 1 retrieves all the relevant pieces from the passed in survMod object, e.g. the survival
+#curves and the true death times of test subjects. Part 2 uses survConcordance to calculate classical concordance measures. Additionally, this is were tied data is handled.
+Concordance = function(survMod, ties = "None", method = "Mean"){
   #Part 1:
   #Being passed an empty model.
   if(is.null(survMod)) return(NULL)
@@ -43,8 +37,6 @@ Concordance = function(survMod, ties = "None",includeCensored =F, method = "Mean
   survivalCurves = survMod[[1]][-1]
   trueDeathTimes = survMod[[2]][,1]
   censorStatus = survMod[[2]][,2]
-  trainingDeathTimes = survMod[[3]]$time
-  trainingCensorStatus = survMod[[3]]$delta
   
   predictMethod = switch(method,
                          Mean = predictMeanSurvivalTimeSpline,
@@ -54,52 +46,11 @@ Concordance = function(survMod, ties = "None",includeCensored =F, method = "Mean
                                        function(index) predictMethod(survivalCurves[,index],
                                                                                      predictedTimes)))
   #Part 2:
-  if(includeCensored){
-    #Here we make the Kaplan-Meier curve and alter the predict function to be linear outside of the last time point of the Kaplan-Meier 
-    #curve.
-    KMCurve = prodlim(Surv(trainingDeathTimes, trainingCensorStatus)~1)
-    KMLinearPredict = function(time){
-      prediction = predict(KMCurve,time)
-      slope = (1-min(KMCurve$surv))/(0 - max(KMCurve$time))
-      predictedProbabiliteis = ifelse(is.na(prediction), pmax(1+time*slope,0), prediction)
-      return(predictedProbabiliteis)
-    }
-    
-    orderDeathTimes = order(trueDeathTimes)
-    sortedDeathTimes = sort(trueDeathTimes)
-    #Each row of the following matrix represents a single individual. Each column is the (ith +1) individual in the testing data.
-    #A value of 1 in the ith row and jth column means that the ith individual (sorted by event time/censored time) is censored and
-    #has a smaller event time than the jth +1 individual. Note that we ignore ties by using sortedDeathTimes[x] < sortedDeathTimes[-x]
-    #as opposed to sortedDeathTimes[x] <= sortedDeathTimes[-x].
-    
-    trueCensMatrix = ldply(lapply(seq_along(sortedDeathTimes), function(x) as.numeric(sortedDeathTimes[x] < sortedDeathTimes[-x]  &
-                                                                                        !censorStatus[orderDeathTimes][x])),rbind)
-    
-    rowIndex = 0
-    estimatedCensMatrix = apply(trueCensMatrix,1,function(x){
-      rowIndex <<- rowIndex+1
-      #Need to add 1 since there are number of individuals -1 columns.
-      toCompare = which(x == 1) +1
-      uncensoredInd = as.logical(censorStatus[orderDeathTimes][toCompare])
-      if(length(toCompare)>0){
-        s2 = KMLinearPredict(sortedDeathTimes[toCompare])
-        s1 = rep(KMLinearPredict(sortedDeathTimes[rowIndex]), length(toCompare))
-        greaterProb = ifelse(uncensoredInd,1-(s2/s1),1 - 0.5*(s2/s1))
-        #If s1 evaluates to 0 then the greater probability should be 1 - they should both already be dead anyway.
-        greaterProb = ifelse(is.nan(greaterProb),1,greaterProb)
-        predictedProb = ifelse(averageSurvivalTimes[orderDeathTimes][rowIndex] <= averageSurvivalTimes[orderDeathTimes][toCompare],
-                               greaterProb,1-greaterProb)
-        x[toCompare-1] = predictedProb
-      }
-      return(x)
-    })
-  }
-  #Part 3:
   #The risk score should be higher for subjects that live shorter (i.e. lower average survival time).
   risk = -1*averageSurvivalTimes
   concordanceInfo = survConcordance(Surv(trueDeathTimes, censorStatus)~ risk)
-  concordantPairs= concordanceInfo$stats[1] + ifelse(includeCensored,sum(estimatedCensMatrix),0)
-  discordantPairs = concordanceInfo$stats[2] + ifelse(includeCensored,sum(trueCensMatrix) - sum(estimatedCensMatrix),0)
+  concordantPairs= concordanceInfo$stats[1] 
+  discordantPairs = concordanceInfo$stats[2] 
   riskTies = concordanceInfo$stats[3]
   timeTies = concordanceInfo$stats[4]
   
