858d7c2edede4998c3c37b5614d2d9db85422833
Humza Haider <humza@haiderstats.com>
2018-11-22 12:02:13 -0700

    Clear up documentation

--- Models/AdditionalMTLRFiles/MTLRHelper.cpp
+++ Models/AdditionalMTLRFiles/MTLRHelper.cpp
@@ -10,10 +10,10 @@ using namespace std;
 //Please see www.cs.cornell.edu/~cnyu/papers/nips11_survival.pdf for relevant information for objective value and gradient calculations.
 
 /*mltr_objVal arguments:
- * (1) params:  The parameters for the MTLR model. These must be sorted with all the biases appearing first followed by the parameters 
+ * (1) params:  The parameters for the MTLR model. These must be sorted with all the biases appearing first followed by the parameters
  * for the first feature at all time points, second feature for all time points, etc. E.g. with m time points we have:
  * b1, b2,...,bm, theta1,1 ,....theta 1,m, theta 2,1, ... theta 2,m, ... theta p,m
- * (2) yval: A matrix with rows indicating the time points and columns being the patients status at that time point. E.g. for 5 time 
+ * (2) yval: A matrix with rows indicating the time points and columns being the patients status at that time point. E.g. for 5 time
  * intervals and for a patient who died in the third interval we would have 0,0,1,1,1 as a column. yval should be a matrix of all these
  * columns. (Should be N x m where N is the number of patients and m is the number of time points).
  * (3) featureValue: These should be the matrix of feature values. A row indicates a single patient (corresponding to the first COLUMN of yval).
@@ -26,23 +26,23 @@ using namespace std;
 // [[Rcpp::export]]
 double mtlr_objVal(NumericVector params, arma::mat yval, arma::mat featureValue, double C1,arma::vec delta) {
   //For the objective value see Equation 3 of the MTLR paper.
-  
+
   //We passed params in as a NumericVector because these are easier to parse and turn into a matrix.
   double valToReturn = 0;
   int N = featureValue.n_rows;
   int m = yval.n_rows;
   Range biasIdx(0,m-1);
-  
+
   NumericVector biasVec = params[biasIdx];
   NumericMatrix thetaMatrix(m,featureValue.n_cols);
   int thetaSize = thetaMatrix.nrow() * thetaMatrix.ncol();
   for (int i = 0; i < thetaSize; i++) {
     thetaMatrix[i] = params[m+i];
-  }  
-  
+  }
+
   arma::mat thetas = as<arma::mat>(thetaMatrix);
   arma::vec biases = as<arma::vec>(biasVec);
-  
+
   arma::vec val(m,arma::fill::ones);
   arma::vec bigVal(m, arma::fill::ones);
   arma::vec secondPieceVal(m, arma::fill::ones);
@@ -61,9 +61,9 @@ double mtlr_objVal(NumericVector params, arma::mat yval, arma::mat featureValue,
     biggestVal = bigVal.max() > 0 ? bigVal.max() : 0;
     secondPieceVal = bigVal - biggestVal;
     double secondPiece = biggestVal + log(sum(exp(secondPieceVal))+exp(-biggestVal));
-    
+
     uncensoredIndex = find(yval.col(i) == 1);
-    
+
     if(uncensoredIndex.size() ==0){
       firstPiece = 0;
     }else{
@@ -75,7 +75,7 @@ double mtlr_objVal(NumericVector params, arma::mat yval, arma::mat featureValue,
     }
 
     valToReturn += firstPiece - secondPiece;
-    
+
   }
   //For the uncensored individuals.
   for(int i=NCens;i<N;i++){
@@ -88,7 +88,7 @@ double mtlr_objVal(NumericVector params, arma::mat yval, arma::mat featureValue,
 
     valToReturn += firstPiece - secondPiece;
   }
-  
+
   valToReturn = (C1/2)*accu(square(thetas)) - valToReturn/N;
   return valToReturn;
 }
@@ -103,25 +103,25 @@ arma::rowvec mtlr_grad(NumericVector params, arma::mat yval, arma::mat featureVa
   double P = featureValue.n_cols;
   int m = yval.n_rows;
   Range biasIdx(0,m-1);
-  
+
   NumericVector biasVec = params[biasIdx];
   NumericMatrix thetaMatrix(m,P);
   int thetaSize = thetaMatrix.nrow() * thetaMatrix.ncol();
   for (int i = 0; i < thetaSize; i++) {
     thetaMatrix[i] = params[m+i];
-  }  
-  
+  }
+
   arma::mat thetas = as<arma::mat>(thetaMatrix);
   arma::vec biases = as<arma::vec>(biasVec);
   arma::vec BiasGradient = arma::zeros<arma::vec>(biases.n_elem);
   arma::mat ThetaGradient = arma::zeros<arma::mat>(thetas.n_rows, thetas.n_cols);
-  
+
   arma::rowvec Xval(P, arma::fill::ones);
   arma::mat  Numer(m,1,arma::fill::ones), ThetaSecond(m,P,arma::fill::ones), toAddWeight(m,P,arma::fill::ones);
   arma::vec toAddBias(m,arma::fill::ones), val(m, arma::fill::ones),bigVal(m, arma::fill::ones),bigValCens(m, arma::fill::ones), BiasSecond(m,arma::fill::ones);
   arma::uvec uncensoredIndex;
   double Denom;
-  
+
   //Censored Piece
   arma::vec BiasCens(m,arma::fill::ones);
   arma::mat NumerCens(m,1,arma::fill::ones), ThetaCens(m,P,arma::fill::ones);
@@ -132,9 +132,9 @@ arma::rowvec mtlr_grad(NumericVector params, arma::mat yval, arma::mat featureVa
     Xval = featureValue.row(i);
     val =  (thetas * Xval.t()) + biases;
     bigVal  = reverse(cumsum(reverse(val)));
-    
+
     uncensoredIndex = find(yval.col(i) == 1);
-    
+
     if(uncensoredIndex.size() ==0){
       bigValCens = bigVal % yval.col(i);
       biggestCensVal = 0;
@@ -145,62 +145,62 @@ arma::rowvec mtlr_grad(NumericVector params, arma::mat yval, arma::mat featureVa
       //we multiply firstPieceVal by ycol to 0 out the other large values in bigVal. Note while this will make exp(0) = 1, it will again be zeroed out by multiplying by y.col(i) again.
       bigValCens %= yval.col(i);
     }
-    
+
     NumerCens= cumsum(yval.col(i) % exp(bigValCens));
     DenomCens = NumerCens[NumerCens.n_rows-1] + exp(-biggestCensVal);
 
     BiasCens = NumerCens/DenomCens;
-    
+
     NumerCens *= Xval;
-    
+
     ThetaCens = NumerCens/DenomCens;
-    
+
     double biggestVal;
     biggestVal = bigVal.max() > 0 ? bigVal.max() : 0;
-  
+
     bigVal -= biggestVal;
-    
+
     Numer= cumsum(exp(bigVal));
     Denom = Numer[Numer.n_rows-1] +exp(-biggestVal);
 
     BiasSecond = Numer/Denom;
-    
+
     Numer *= Xval;
     ThetaSecond = Numer/Denom;
-    
+
     toAddBias =  BiasCens - BiasSecond;
     toAddWeight =  ThetaCens - ThetaSecond;
     BiasGradient += toAddBias;
 
     ThetaGradient += toAddWeight;
   }
-  
+
   //Uncensored Piece
   for(int i=NCens;i<N;i++){
     Xval = featureValue.row(i);
     val =  (thetas * Xval.t()) + biases;
-    
+
     bigVal  = reverse(cumsum(reverse(val)));
     double biggestVal;
     biggestVal = bigVal.max() > 0 ? bigVal.max() : 0;
     bigVal -= biggestVal;
-    
+
     Numer= cumsum(exp(bigVal));
     Denom = Numer[Numer.n_rows-1] +exp(-biggestVal);
     BiasSecond = Numer/Denom;
-    
+
     Numer *= Xval;
     ThetaSecond = Numer/Denom;
-    
+
     toAddBias =  sum(yval.col(i) - BiasSecond.each_col(), 1);
     toAddWeight =  yval.col(i) * Xval - ThetaSecond;
     BiasGradient += toAddBias;
     ThetaGradient += toAddWeight;
   }
-  
+
   BiasGradient.transform( [N](double val) { return (val * (double)(-1/N)); } );
   ThetaGradient.transform( [N](double val) { return (val * (double)(1/N)); } );
-  
+
   ThetaGradient = C1*thetas - ThetaGradient;
   arma::vec ThetaFlat = vectorise(ThetaGradient);
   return arma::join_cols<arma::mat>(BiasGradient, ThetaFlat).t();
@@ -212,26 +212,26 @@ arma::rowvec mtlr_grad(NumericVector params, arma::mat yval, arma::mat featureVa
 arma::mat mtlr_predict(NumericVector params,arma::mat featureValue){
   double N = featureValue.n_rows;
   double P = featureValue.n_cols;
-  
-  
+
+
   double m = params.size()/(P+1); // +1 to account for bias terms.
   Range biasIdx(0,m-1);
-  
+
   NumericVector biasVec = params[biasIdx];
   NumericMatrix thetaMatrix(m,featureValue.n_cols);
   int thetaSize = thetaMatrix.nrow() * thetaMatrix.ncol();
   for (int i = 0; i < thetaSize; i++) {
     thetaMatrix[i] = params[m+i];
-  }  
+  }
   arma::mat thetas = as<arma::mat>(thetaMatrix);
   arma::vec biases = as<arma::vec>(biasVec);
-  
-  
+
+
   arma::mat resMat = thetas*featureValue.t();
   resMat.each_col() += biases;
   arma::mat B = exp(reverse(cumsum(reverse(resMat))));
   arma::mat Ones = arma::ones<arma::mat>(1,N);
-  
+
   B.insert_rows(B.n_rows,Ones);
   arma::rowvec BDenoms = sum(B);
   arma::mat resultsMatrix = reverse(cumsum(reverse(B.each_row()/BDenoms)));
